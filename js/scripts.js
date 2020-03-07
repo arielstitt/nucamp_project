7a2cbd917f87a79c110e5c1a98151127174448cb
Ariel Stitt <arielstitt@Skills-Macbook-Pro-37.local>
2020-03-07 10:06:19 -0500

    added dist folder and fixed issue in package json

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,10 @@ $(function () {
             $("#carouselButton").children("i").addClass("fa-pause");
         }
     });
+    $('#reserveButton').click(function () {
+        $('#reservemodal').modal('show');
+    });
+    $('#loginButton').click(function () {
+        $('#loginModal').modal('show');
+    });
 });
